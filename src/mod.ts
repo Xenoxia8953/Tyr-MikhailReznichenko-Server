98ff5f77fac7aa9bf70953204a4974ff9a90be46
Tyrian <thomasjosephward@gmail.com>
2025-01-23 21:32:34 +0000

    Fixed stuff?
    Fixed stuff?

--- src/mod.ts
+++ src/mod.ts
@@ -31,7 +31,8 @@ class MikhailReznichenko   implements IPreSptLoadMod, IPostDBLoadMod
     public modIdList: string[] = [];
     public configToClient = "/tyrian/mikhail_reznichenko/config_to_client";
 
-    constructor() {
+    constructor() 
+    {
         this.mod = "Tyr-MikhailReznichenko"; 
     }
 
@@ -83,20 +84,28 @@ class MikhailReznichenko   implements IPreSptLoadMod, IPostDBLoadMod
         this.traderHelper.addTraderToLocales(traderJson, tables, traderJson.name, "MikhailReznichenko", traderJson.nickname, traderJson.location, "Welcome, friend. Here, you'll only find strong and honest wood.");
         
         this.modHelper.init(container, InitStage.POST_DB_LOAD);
-        for (const item of customItems) {
+        for (const item of customItems) 
+        {
             this.addSimpleItemToDb(item);
             this.addSimpleItemToTraderAssort(item);
-            this.modIdList.push(item.id);
         }
         this.logger.debug(`[${this.mod}] items in modIdList: ${this.modIdList.length > 0 ? this.modIdList.join(", ") : "No items added"}`);
         this.logger.debug(`[${this.mod}] postDb Loaded`);
     }
 
-    static onConfigToClient(url: string, info: any, sessionId: string, output: string, helper: ModHelper, modIdList: string[]): string {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    static onConfigToClient(url: string, info: any, sessionId: string, output: string, helper: ModHelper): string 
+    {
+        const modIdList: string[] = [];
+        for (const item of customItems) 
+        {
+            modIdList.push(item.id);
+        }
         return JSON.stringify(modIdList);
     }
 	
-    private addSimpleItemToDb(itemTemplate: SimpleItem): void {
+    private addSimpleItemToDb(itemTemplate: SimpleItem): void 
+    {
         const itemClone: ITemplateItem = FileUtils.jsonClone<ITemplateItem>(this.modHelper.dbItems[itemTemplate.itemType]);
 		
         itemClone._id = itemTemplate.id;
@@ -109,39 +118,47 @@ class MikhailReznichenko   implements IPreSptLoadMod, IPostDBLoadMod
         itemClone._props.Weight = itemTemplate.weight;
         itemClone._props.Prefab.path = itemTemplate.bundlePath;
 		
-        if (itemTemplate.itemType === "5df8a4d786f77412672a1e3b"){
+        if (itemTemplate.itemType === "5df8a4d786f77412672a1e3b")
+        {
             // Builders Backpack
             itemClone._props.Grids[0]._props.cellsH = 7
             itemClone._props.Grids[0]._props.cellsV = 128
         }
-        if (itemTemplate.itemType === "5c0a840b86f7742ffa4f2482"){
+        if (itemTemplate.itemType === "5c0a840b86f7742ffa4f2482")
+        {
             // Container Items
-            if (itemTemplate.id === "678ff6a08def9feca215636e"){
+            if (itemTemplate.id === "678ff6a08def9feca215636e")
+            {
                 //Large Ammo Box
                 itemClone._props.Grids[0]._props.cellsH = 18
                 itemClone._props.Grids[0]._props.cellsV = 12
             }
-            if (itemTemplate.id === "678ff754fa2aee130bf269da"){
+            if (itemTemplate.id === "678ff754fa2aee130bf269da")
+            {
                 //Stubby Ammo Box
                 itemClone._props.Grids[0]._props.cellsH = 14
                 itemClone._props.Grids[0]._props.cellsV = 14
             }
-            if (itemTemplate.id === "678ff749a1b18d76f8bb08d0"){
+            if (itemTemplate.id === "678ff749a1b18d76f8bb08d0")
+            {
                 //Small Ammo Box
                 itemClone._props.Grids[0]._props.cellsH = 12
                 itemClone._props.Grids[0]._props.cellsV = 8
             }
-            if (itemTemplate.id === "678ff771ed3fba9e8998c76f"){
+            if (itemTemplate.id === "678ff771ed3fba9e8998c76f")
+            {
                 //Large Weapon Case
                 itemClone._props.Grids[0]._props.cellsH = 18
                 itemClone._props.Grids[0]._props.cellsV = 12
             }
-            if (itemTemplate.id === "678ff7ec91e978af07400932"){
+            if (itemTemplate.id === "678ff7ec91e978af07400932")
+            {
                 //Massive Supply Case
                 itemClone._props.Grids[0]._props.cellsH = 24
                 itemClone._props.Grids[0]._props.cellsV = 24
             }
-            if (itemTemplate.id === "678ff970bbb8bdc6515a87b2"){
+            if (itemTemplate.id === "678ff970bbb8bdc6515a87b2")
+            {
                 //Fridge
                 itemClone._props.Grids[0]._props.cellsH = 16
                 itemClone._props.Grids[0]._props.cellsV = 16
@@ -156,7 +173,8 @@ class MikhailReznichenko   implements IPreSptLoadMod, IPostDBLoadMod
             Price: itemTemplate.fleaPrice,
         });
 
-        for (const langKey in this.modHelper.dbLocales.global) {
+        for (const langKey in this.modHelper.dbLocales.global) 
+        {
             const locale = this.modHelper.dbLocales.global[langKey];
             locale[`${itemTemplate.id} Name`] = itemTemplate.name;
             locale[`${itemTemplate.id} ShortName`] = itemTemplate.name;
@@ -164,7 +182,8 @@ class MikhailReznichenko   implements IPreSptLoadMod, IPostDBLoadMod
         }
     }
 	
-    private addSimpleItemToTraderAssort(itemTemplate: SimpleItem): void {
+    private addSimpleItemToTraderAssort(itemTemplate: SimpleItem): void 
+    {
         const trader = this.modHelper.dbTraders[this.getTraderId("mikhail")];
 
         const barter: IBarterScheme = {
@@ -190,11 +209,13 @@ class MikhailReznichenko   implements IPreSptLoadMod, IPostDBLoadMod
         //trader.assort.loyal_level_items[itemTemplate.assortId] = itemTemplate.loyaltyLevel;
     }
 	
-    getTraderId(traderName: string): string {
+    getTraderId(traderName: string): string 
+    {
         return ModHelper.traderIdsByName[traderName] ?? traderName;
     }
 
-    getCurrencyId(currencyName: string): string {
+    getCurrencyId(currencyName: string): string 
+    {
         return ModHelper.currencyIdsByName[currencyName] ?? currencyName;
     }
 }
